61abbf91bd3eac00229dcbf5efaa01eb1f3a7176
VenusNguyen <65570643+VenusNguyen@users.noreply.github.com>
2020-06-05 20:31:26 -0700

    Update Animation.h

--- Animation.h
+++ Animation.h
@@ -12,18 +12,22 @@ class Animation : public sf::Drawable {
 public:
     Animation();
     Animation(std::string fileName);
-    Animation(std::string fileName, int left, int top, int width, int height);
-    void setTextureRect(int left, int top, int width, int height);
+    Animation(std::string fileName, float left, float top, int row, int column);
+    void setTextureRect(float left, float top, float width, float height);
     void setScale(sf::Vector2f size);
     void setTime(float time);
     void setPosition(float x, float y);
     void animate();
     void rotate(float angle);
+    void move(sf::Vector2f offset);
+    void goToRow(int row);
+    void setColumn(int column);
     virtual void draw(sf::RenderTarget &window, sf::RenderStates state) const;
 
 private:
     float _time = 1000.f;
-    int _left{};
+    float _left;
+    float _width, _height;
     sf::Texture texture;
     sf::Clock clock;
     sf::IntRect sourceSprite;
