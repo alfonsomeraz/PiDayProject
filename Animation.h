31cd1dc8d97cc7bc0bdb2fada30702f1c5c78ff2
yxchang1 <65572101+yxchang1@users.noreply.github.com>
2020-06-08 16:58:28 -0700

    Update Animation.h

--- Animation.h
+++ Animation.h
@@ -1,3 +1,6 @@
+//
+// Edited by Yu-Chung Chang on 6/8/20
+//
 // Edited by Alyssa Ng on 6/7/20.
 //
 // Created by Venus Nguyen on 5/31/20.
@@ -21,7 +24,11 @@ public:
     void animate();
     void setRotation(float angle);
     void rotate(float rotateBy, float targetAngle);
+    void rotateSwing(float rotateBy, float targetAngle);
     void move(sf::Vector2f offset, sf::Vector2f targetPosition);
+    void moveByGravitation(sf::Vector2f offset, sf::Vector2f targetPosition);
+    void moveVertically(sf::Vector2f offset, sf::Vector2f targetPosition);
+    void moveHoriz(sf::Vector2f offset, sf::Vector2f targetPosition);
     void goToRow(int row);
     void setColumn(int column);
     virtual void draw(sf::RenderTarget &window, sf::RenderStates state) const;
