23afd4719d18901dfc04741b7c66ea191697d0ec
VenusNguyen <65570643+VenusNguyen@users.noreply.github.com>
2020-06-10 17:31:18 -0700

    Update Animation.cpp

--- Animation.cpp
+++ Animation.cpp
@@ -1,8 +1,4 @@
 //
-// Edited by Yu-Chung Chang on 6/8/20
-//
-// Edited by Alyssa Ng on 6/7/20.
-//
 // Created by Venus Nguyen on 5/31/20.
 //
 
@@ -12,23 +8,24 @@
 Animation::Animation()
 {}
 
-Animation::Animation(std::string fileName) : fileName(fileName)
+Animation::Animation(std::string fileName) : _fileName(fileName)
 {
-    if (!texture.loadFromFile(fileName))
+    if (!texture.loadFromFile(_fileName))
         exit(1);
     sprite.setTexture(texture);
     sprite.setTextureRect(sourceSprite);
 }
 
-Animation::Animation(std::string fileName, float left, float top, int row, int column)
-    : fileName(fileName), left(left)
+Animation::Animation(std::string fileName, float left, float top, int row, int column) : _fileName(fileName), left(left)
 {
-    if (!texture.loadFromFile(fileName))
+    if (!texture.loadFromFile(_fileName))
         exit(1);
     width = texture.getSize().x / column;
     height = texture.getSize().y / row;
+
     sprite.setTexture(texture);
     setTextureRect(left, top, width, height);
+    this -> left = left;
 }
 
 void Animation::setTextureRect(float left, float top, float width, float height)
@@ -38,11 +35,22 @@ void Animation::setTextureRect(float left, float top, float width, float height)
     sourceSprite.width = width;
     sourceSprite.height = height;
     sprite.setTextureRect(sourceSprite);
+    this -> left = left;
+}
+
+void Animation::setTextureRect(float left, float top, int row, int column)
+{
+    width = texture.getSize().x / column;
+    height = texture.getSize().y / row;
+
+    setTextureRect(left, top, width, height);
+    sprite.setTexture(texture);
+    this -> left = left;
 }
 
 void Animation::setTime(float time)
 {
-    this->time = time;
+    _time = time;
 }
 
 void Animation::setPosition(float x, float y)
@@ -55,9 +63,19 @@ void Animation::setOrigin(float x, float y)
     sprite.setOrigin(x, y);
 }
 
+sf::Vector2<unsigned int> Animation::getTextureSize()
+{
+    return texture.getSize();
+}
+
+sf::IntRect& Animation::getTextureRect()
+{
+    return sourceSprite;
+}
+
 void Animation::animate()
 {
-    if (clock.getElapsedTime().asMilliseconds() > time)
+    if (clock.getElapsedTime().asMilliseconds() > _time)
     {
         if (sourceSprite.left >= (texture.getSize().x - sourceSprite.width))
             sourceSprite.left = left;
@@ -69,68 +87,40 @@ void Animation::animate()
     }
 }
 
-void Animation::scale(float factorX, float factorY)
+void Animation::animateLoop()
 {
-    sf::Vector2f scale = sprite.getScale();
-    sprite.setScale(factorX, factorY);
-}
-
-void Animation::setRotation(float angle) 
-{
-    sprite.setRotation(angle);
-}
-
-void Animation::rotate(float rotateBy, float targetAngle) // Function WILL infinite loop if if targetAngle % rotateBy != 0
-{
-    if (sprite.getRotation() != targetAngle)
-        sprite.rotate(rotateBy);
-}
-
-void Animation::rotateSwing(float rotateBy, float targetAngle)
-{
-    if (sprite.getRotation() < targetAngle)
-        sprite.rotate(rotateBy);
-    /*
-    if (clock.getElapsedTime().asSeconds() > .25f)
+    if (clock.getElapsedTime().asMilliseconds() > 100.f)
     {
-        if (sprite.getRotation() < targetAngle)
-            sprite.rotate(5);
-
+        sourceSprite.left += sourceSprite.width;
+        sprite.setTextureRect(sourceSprite);
         clock.restart();
     }
-     */
-
 }
 
-void Animation::move(sf::Vector2f offset, sf::Vector2f targetPosition)
+//void Animation::rotation(float targetAngle)
+//{
+//    if (clock.getElapsedTime().asSeconds() > .25f)
+//    {
+//        if (sprite.getRotation() < targetAngle)
+//            sprite.rotate(5);
+//
+//        clock.restart();
+//    }
+//}
+
+void Animation::setScale(sf::Vector2f size)
 {
-    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
-            sprite.move(offset);
+    sprite.setScale(size);
 }
 
-void Animation::moveByGravity(sf::Vector2f offset, sf::Vector2f targetPosition)
+void Animation::rotate(float angle)
 {
-    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
-        //sprite.move(offset);
-        //sf::Vector2f spritePos = sprite.getPosition();
-        sprite.setPosition(sprite.getPosition().x + .1, (sprite.getPosition().x + .1) / 1920.0 * (sprite.getPosition().x + .1) / 1920.0 * 1080.0);
+    sprite.rotate(angle);
 }
 
-void Animation::moveVertically(sf::Vector2f offset, sf::Vector2f targetPosition)
+void Animation::move(sf::Vector2f offset)
 {
-    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
-        //sprite.move(offset);
-        //sf::Vector2f spritePos = sprite.getPosition();
-        sprite.setPosition(sprite.getPosition().x , (sprite.getPosition().y + 1) );
-}
-
-
-void Animation::moveHoriz(sf::Vector2f offset, sf::Vector2f targetPosition)
-{
-    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
-        //sprite.move(offset);
-        //sf::Vector2f spritePos = sprite.getPosition();
-        sprite.setPosition(sprite.getPosition().x + .1 , (sprite.getPosition().y ) );
+    sprite.move(offset);
 }
 
 void Animation::goToRow(int row)
@@ -145,7 +135,37 @@ void Animation::setColumn(int column)
     sprite.setTextureRect(sourceSprite);
 }
 
+sf::Vector2f Animation::getPosition()
+{
+    return sprite.getPosition();
+}
+
 void Animation::draw(sf::RenderTarget &window, sf::RenderStates state) const
 {
     window.draw(sprite);
+}
+
+void Animation::moveByGravitation(sf::Vector2f offset, sf::Vector2f targetPosition)
+{
+    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
+        //sprite.move(offset);
+        //sf::Vector2f spritePos = sprite.getPosition();
+        sprite.setPosition(sprite.getPosition().x + 1, (sprite.getPosition().x + 1) / 1920.0 * (sprite.getPosition().x + 1) / 1920.0 * 1080.0);
+}
+
+void Animation::moveVertically(sf::Vector2f offset, sf::Vector2f targetPosition)
+{
+//    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
+        //sprite.move(offset);
+        //sf::Vector2f spritePos = sprite.getPosition();
+        sprite.setPosition(sprite.getPosition().x , (sprite.getPosition().y + 1) );
+}
+
+
+void Animation::moveHoriz(sf::Vector2f offset, sf::Vector2f targetPosition)
+{
+    if (sprite.getPosition().x < targetPosition.x && sprite.getPosition().y < targetPosition.y)
+        //sprite.move(offset);
+        //sf::Vector2f spritePos = sprite.getPosition();
+        sprite.setPosition(sprite.getPosition().x + .1 , (sprite.getPosition().y ) );
 }
