79c7a596f8afe5863045e5afc9f3a65d87716d3d
VenusNguyen <65570643+VenusNguyen@users.noreply.github.com>
2020-06-10 17:38:33 -0700

    Update WordGraphic.h

--- WordGraphic.h
+++ WordGraphic.h
@@ -2,37 +2,99 @@
 // Created by Venus Nguyen on 5/28/20.
 //
 
-#ifndef FINALPROJECT_WORDGRAPHIC_H
-#define FINALPROJECT_WORDGRAPHIC_H
+#include "WordGraphic.h"
 #include "Word.h"
-#include <SFML/Graphics.hpp>
 
 
-class WordGraphic : public sf::Drawable{
-public:
-    WordGraphic();
-    WordGraphic(std::string word, float x, float y, int size);
-    void setWord(std::string word);
-    bool revealLetter(char letter);
-    void setSize(int size);
-    void setPosition(sf::Vector2f pos);
-    void setPosition(float x, float y);
-    bool isReveal() const;
-    bool checkLetter(char letter) const;
-    virtual void draw(sf::RenderTarget &window, sf::RenderStates state) const;
-    void addEvent(sf::RenderWindow &window, sf::Event event);
-    enum states { BAD_LETTER, GOOD_LETTER};
-    void changeState(states state);
-    states getState();
-private:
+WordGraphic::WordGraphic()
+{
+    if (!font.loadFromFile("OpenSans-Regular.ttf"))
+        exit(1);
+    text.setFillColor(sf::Color::Black);
+    text.setLetterSpacing(5);
+    text.setFont(font);
+}
 
-    Word _word;
-    sf::Text text;
-    sf::Font font;
-    int _size = 30;
-    sf::Vector2f _pos;
-    states state = GOOD_LETTER;
-};
+WordGraphic::WordGraphic(std::string word, float x, float y, int size) : _word(word)
+{
+    setWord(word);
+    setSize(size);
+    setPosition(x, y);
+}
 
+void WordGraphic::setSize(int size)
+{
+    _size = size;
+    text.setCharacterSize(_size);
+}
 
-#endif //FINALPROJECT_WORDGRAPHIC_H
+void WordGraphic::setPosition(sf::Vector2f pos)
+{
+    _pos = pos;
+    text.setPosition(_pos);
+}
+
+void WordGraphic::setPosition(float x, float y)
+{
+    _pos.x = x;
+    _pos.y = y;
+    text.setPosition(_pos);
+}
+
+void WordGraphic::setWord(std::string word)
+{
+    _word = word;
+    if (!font.loadFromFile("OpenSans-Regular.ttf"))
+        exit(1);
+    text.setFillColor(sf::Color::Black);
+    text.setLetterSpacing(5);
+    text.setFont(font);
+    text.setString(_word.getWord());
+}
+
+bool WordGraphic::isReveal() const
+{
+    return _word.isReveal();
+}
+
+bool WordGraphic::checkLetter(char letter) const
+{
+    return _word.checkLetter(letter);
+}
+
+void WordGraphic::draw(sf::RenderTarget &window, sf::RenderStates state) const
+{
+    window.draw(text);
+}
+
+bool WordGraphic::revealLetter(char letter)
+{
+    bool b = _word.revealLetter(letter);
+    text.setString(_word.getWord());
+    return b;
+}
+
+void WordGraphic::addEvent(sf::RenderWindow &window, sf::Event event)
+{
+    if (event.type == sf::Event::TextEntered)
+    {
+        if (isalpha(event.key.code))
+        {
+            char c = event.key.code;
+            if(!revealLetter(c))
+            {
+                state = BAD_LETTER;
+            }
+        }
+    }
+}
+
+void WordGraphic::changeState(states state)
+{
+    this->state = state;
+}
+WordGraphic::states WordGraphic::getState()
+{
+    return state;
+}
+
